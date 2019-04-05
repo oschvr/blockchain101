e0b0dcb1258e2d88f0532d5d6263e29cf4990d57
oschvr <oscarchavezromero@gmail.com>
2019-04-05 00:46:03 -0600

    Make something useful with this, boys

---
+++ Purchase.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+contract HelloWorld {
+
+  string saySomething;
+
+  constructor() public {
+    saySomething = ”Hola FLISOL!";
+  }
+
+  function speak() constant public returns(string isSaying) {
+    return saySomething;
+  }
+
+  function sayOtherThing(string somethingElse) public returns(bool success) {
+    saySomething = somethingElse;
+    return true;
+  }
+}
+
